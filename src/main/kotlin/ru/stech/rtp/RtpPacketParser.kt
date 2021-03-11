b66277ce7d1093fa26e8a04af96afe7cadb018f9
Aleksandr Artamonov <estgold@yandex.ru>
2021-03-11 17:43:26 +0300

    Added RtpPacketParser

---
+++ src/main/kotlin/ru/stech/rtp/RtpPacketParser.kt
@@ -0,0 +1,19 @@
+package ru.stech.rtp
+
+import ru.stech.g711.compressor.CompressInputStream
+import java.io.ByteArrayInputStream
+
+class RtpPacketParser {
+    fun parseBytes(bytes: ByteArray, headers: RtpPacketHeaders, useALaw: Boolean): Array<RtpPacket> {
+        val cis = CompressInputStream(ByteArrayInputStream(bytes), useALaw)
+        val result = mutableListOf<RtpPacket>()
+        cis.use { compressInputStream: CompressInputStream ->
+            val payload = compressInputStream.readNBytes(60)
+            val packet = RtpPacket(headers)
+            packet.CSRCCount = (Math.ceil((payload.size / 4).toDouble()).toInt().toByte())
+            packet.setPayload(payload)
+            result.add(packet)
+        }
+        return result.toTypedArray()
+    }
+}
